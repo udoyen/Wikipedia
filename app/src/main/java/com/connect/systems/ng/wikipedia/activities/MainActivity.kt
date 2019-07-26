9e0efcfc612a26a7782f7e8abdb069472a95cbd4
udoyen <datameshprojects@gmail.com>
2019-07-26 21:27:00 +0100

    done with UI design

--- app/src/main/java/com/connect/systems/ng/wikipedia/activities/MainActivity.kt
+++ app/src/main/java/com/connect/systems/ng/wikipedia/activities/MainActivity.kt
@@ -38,6 +38,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 //        val navView: BottomNavigationView = findViewById(R.id.nav_view)
         nav_view.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
 
